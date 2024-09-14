3f887749c16547b11fb70dddbc025f6c9228ed51
Marcos Juarez <>
2024-09-14 16:19:11 -0600

    Update: APIConnection

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+import secret
+import requests
 
 app = Flask( __name__ , static_folder='../dist/static',template_folder = '../dist')
 cors = CORS( app, origins = '*' )
@@ -8,13 +10,10 @@ cors = CORS( app, origins = '*' )
 def index(id = 0):
     return render_template("index.html")
 
-@app.route ( "/api/users", methods = [ 'GET' ] )
+@app.route  ( "/api/users", methods =  [ 'GET'  ] )
 
 def users():
-    # users = {'@markz' : 'Marcos J', '@markz2' : 'Marcos J'}
-    users = [ { 'userName': '@dgmnzrd_', 'name': 'ing. caguama indio', 'isFollowing': True }, 
-              { 'userName': '@leozglz', 'name': 'leo', 'isFollowing': True },
-              { 'userName': '@marcos_jn7', 'name': 'Marcos Jn', 'isFollowing': False } ]
-    return users
-    
+    pruebaget = requests.get(f'http://api.nessieisreal.com/customers?key={secret.API_KEY_LOCAL}')
+    return pruebaget.json()
+
 app.run( debug = True, port = 5555 )
